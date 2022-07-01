28949b2e67a0510dc1da386d476eb6ae7b3ef3dd
Ranil <rnyuskaev@gmail.com>
2022-07-01 19:02:33 +0500

    добавил светлую/темную тему

---
+++ theme.js
@@ -0,0 +1,62 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ThemeButton = function (_React$Component) {
+  _inherits(ThemeButton, _React$Component);
+
+  function ThemeButton(props) {
+    _classCallCheck(this, ThemeButton);
+
+    var _this = _possibleConstructorReturn(this, (ThemeButton.__proto__ || Object.getPrototypeOf(ThemeButton)).call(this, props));
+
+    _this.state = { DarkTheme: false };
+    return _this;
+  }
+
+  _createClass(ThemeButton, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      if (this.state.DarkTheme) {
+        document.body.className = 'bg-dark text-white';
+
+        return React.createElement(
+          "button",
+          { "class": "btn", onClick: function onClick() {
+              return _this2.setState({ DarkTheme: !_this2.state.DarkTheme });
+            } },
+          React.createElement(
+            "svg",
+            { xmlns: "http://www.w3.org/2000/svg", width: "32", height: "32", fill: "#fff", "class": "bi bi-brightness-high", viewBox: "0 0 16 16" },
+            React.createElement("path", { d: "M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" })
+          )
+        );
+      } else {
+        document.body.className = 'bg-light';
+
+        return React.createElement(
+          "button",
+          { "class": "btn", onClick: function onClick() {
+              return _this2.setState({ DarkTheme: !_this2.state.DarkTheme });
+            } },
+          React.createElement(
+            "svg",
+            { xmlns: "http://www.w3.org/2000/svg", width: "32", height: "32", fill: "#000", "class": "bi bi-moon", viewBox: "0 0 16 16" },
+            React.createElement("path", { d: "M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" })
+          )
+        );
+      }
+    }
+  }]);
+
+  return ThemeButton;
+}(React.Component);
+
+var domContainer = document.querySelector('#theme');
+ReactDOM.render(React.createElement(ThemeButton, null), domContainer);
